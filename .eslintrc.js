8bae7327280f7a4682732c4c5580987900a643b9
wolf <1510156610@qq.com>
2019-09-15 11:25:39 +0800

    master change the ker word

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "@vue/prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/no-parsing-error": [2, { "x-invalid-end-tag": false }],
+    'vue/no-parsing-error': 'off',
+    'vue/no-unused-vars': 'warning'
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+};
